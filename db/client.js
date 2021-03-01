b63c1ffb2d40d3e7f6311e865f8ad3f2025236a3
Aaron Pollack <apollack0722@gmail.com>
2021-03-01 15:49:29 -0600

    initial commit and set up db client

--- db/client.js
+++ db/client.js
@@ -1,2 +1,9 @@
 // build and export your unconnected client here
 
+const { Client } = require('pg');
+
+const client = new Client('postres://localhost:5432/fitness_dev')
+
+module.exports = {
+    client,
+}
