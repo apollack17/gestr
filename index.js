72e9a390c4956faaf4d1f1bf94a2b2ced69153e8
Aaron Pollack <apollack0722@gmail.com>
2021-03-10 15:18:01 -0600

    index filled out and api tests started

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// create the express server here
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const PORT = 3000 || process.env
+const {client} = require("./db")
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', require('./api')) 
+app.get('/', function(req, res, next) {
+    var e = new Error('Request failed with status code 404');
+    e.status = 404;
+    next(e);
+});
+app.get('/', function(req, res, next) {
+    var e = new Error("Request failed with status code 500");
+    e.status = 500;
+    next(e);
+});
+app.listen(PORT, ()=> {
+    console.log("server is up on ", PORT)
+    client.connect();
+})// create the express server here
