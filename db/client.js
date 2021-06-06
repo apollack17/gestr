58e558281fd9d11874439acd1403e7acb37410ba
Aaron Pollack <apollackdev@gmail.com>
2021-06-06 18:56:08 -0500

    set connection variables

--- db/client.js
+++ db/client.js
@@ -2,6 +2,6 @@
 
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/fitness_dev')
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 module.exports = client
