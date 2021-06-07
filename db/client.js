95b78ee84ee063477547eb0490b5858349abd4ba
Aaron Pollack <apollackdev@gmail.com>
2021-06-06 19:15:13 -0500

    heroku ssl rejection changes

--- db/client.js
+++ db/client.js
@@ -2,6 +2,10 @@
 
 const { Client } = require('pg');
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/fitnessdev');
-
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 module.exports = client
